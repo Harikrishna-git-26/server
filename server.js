81e68da8c42861b2602ae3c0c838afe5d94ed8a5
Harikrishna-git-26 <tiworkpart2@gmail.com>
2025-10-26 15:29:09 +0530

    "S3"

--- server.js
+++ server.js
@@ -15,46 +15,39 @@ app.get("/healthz", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-const USE_HTTPS = process.env.USE_HTTPS === "true";
 
-let server;
-if (
-  USE_HTTPS &&
-  fs.existsSync("./certs/key.pem") &&
-  fs.existsSync("./certs/cert.pem")
-) {
-  const key = fs.readFileSync("./certs/key.pem");
-  const cert = fs.readFileSync("./certs/cert.pem");
-  server = https.createServer({ key, cert }, app);
-} else {
-  server = http.createServer(app);
-}
+let server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+  cors: {origin: "*", methods: ["GET", "POST"]}
 });
+
+// --- Room/Group logic: ---
+const peers = new Map();
+// groupPeers maps: shortId -> Set of connected peer shortIds (starts as just self)
+const groupPeers = new Map();
 
 function generateShortId() {
   return Math.random().toString(36).substring(2, 7).toUpperCase();
 }
 
-const peers = new Map();
-const socketToShort = new Map();
-let hostShortId = null;
+// Only send peer list of explicit connections (including self)
+function buildPeerList(shortId) {
+  const group = groupPeers.get(shortId) || new Set([shortId]);
+  return Array.from(group).map(id => {
+    const peer = peers.get(id);
+    if (!peer) return null;
+    return {
+      id: peer.shortId,
+      name: peer.name,
+      camOn: peer.camOn,
+      micOn: peer.micOn,
+      streamId: peer.streamId ?? null,
+    };
+  }).filter(Boolean);
+}
 
-const buildPeerList = () =>
-  Array.from(peers.values()).map((p) => ({
-    id: p.shortId,
-    name: p.name,
-    camOn: p.camOn,
-    micOn: p.micOn,
-    streamId: p.streamId ?? null,
-  }));
-
-io.on("connection", (socket) => {
+io.on("connection", socket => {
   const shortId = generateShortId();
   peers.set(shortId, {
     socketId: socket.id,
@@ -64,122 +57,121 @@ io.on("connection", (socket) => {
     micOn: false,
     streamId: null,
   });
-  socketToShort.set(socket.id, shortId);
-
-  if (!hostShortId) hostShortId = shortId;
+  groupPeers.set(shortId, new Set([shortId]));
 
   socket.emit("connect-success", { id: shortId });
-  io.emit("host", { id: hostShortId });
-  io.emit("update-peers", buildPeerList());
+  socket.emit("update-peers", buildPeerList(shortId));
 
+  // Name
   socket.on("set-name", ({ name }) => {
     const info = peers.get(shortId);
     if (info) {
       info.name = name;
       peers.set(shortId, info);
-      io.emit("peer-updated", {
+      socket.emit("peer-updated", {
         id: shortId,
         name,
         camOn: info.camOn,
         micOn: info.micOn,
       });
-      io.emit("update-peers", buildPeerList());
+      socket.emit("update-peers", buildPeerList(shortId));
     }
   });
 
+  // Cam/mic
   socket.on("update-status", (status) => {
     const info = peers.get(shortId);
     if (info) {
       info.camOn = !!status.camOn;
       info.micOn = !!status.micOn;
       peers.set(shortId, info);
-      io.emit("peer-updated", {
-        id: shortId,
-        name: info.name,
-        camOn: info.camOn,
-        micOn: info.micOn,
-      });
-      io.emit("update-peers", buildPeerList());
+      // Inform only group peers
+      for (const peerId of (groupPeers.get(shortId) || [])) {
+        const targetPeer = peers.get(peerId);
+        if (targetPeer)
+          io.to(targetPeer.socketId).emit("peer-updated", {
+            id: shortId,
+            name: info.name,
+            camOn: info.camOn,
+            micOn: info.micOn,
+          });
+      }
     }
   });
 
+  // Connect to another by ID -- both gain each other as peers
+  socket.on("connect-peer", (targetId) => {
+    if (!targetId || !peers.has(targetId) || targetId === shortId) return;
+    groupPeers.get(shortId).add(targetId);
+    groupPeers.get(targetId).add(shortId);
+    // update both, so peer lists reflect new group
+    [shortId, targetId].forEach(id => {
+      const p = peers.get(id);
+      if (p) io.to(p.socketId).emit("update-peers", buildPeerList(id));
+    });
+  });
+
+  // Offer/Answer signaling – only forward to peers in your group
   socket.on("offer", ({ to, signal, name }) => {
-    const toPeer = peers.get(to);
-    if (toPeer) {
-      io.to(toPeer.socketId).emit("offer", {
-        from: shortId,
-        signal,
-        name,
-      });
+    if (groupPeers.get(shortId)?.has(to)) {
+      const toPeer = peers.get(to);
+      if (toPeer) io.to(toPeer.socketId).emit("offer", { from: shortId, signal, name });
     }
   });
-
   socket.on("answer", ({ to, signal }) => {
-    const toPeer = peers.get(to);
-    if (toPeer) {
-      io.to(toPeer.socketId).emit("answer", {
-        from: shortId,
-        signal,
-      });
+    if (groupPeers.get(shortId)?.has(to)) {
+      const toPeer = peers.get(to);
+      if (toPeer) io.to(toPeer.socketId).emit("answer", { from: shortId, signal });
     }
   });
 
+  // Chat and video signaling (as above, only for group)
   socket.on("send-message", ({ to, msg, name }) => {
-    const toPeer = peers.get(to);
-    if (toPeer) {
-      io.to(toPeer.socketId).emit("receive-message", {
-        from: shortId,
-        name: name || "",
-        msg,
-      });
+    if (groupPeers.get(shortId)?.has(to)) {
+      const toPeer = peers.get(to);
+      if (toPeer) io.to(toPeer.socketId).emit("receive-message", { from: shortId, name: name ?? "", msg });
     }
   });
-
   socket.on("send-video", ({ to, url, action, time }) => {
-    const toPeer = peers.get(to);
-    if (toPeer) {
-      io.to(toPeer.socketId).emit("receive-video", { url, action, time });
+    if (groupPeers.get(shortId)?.has(to)) {
+      const toPeer = peers.get(to);
+      if (toPeer) io.to(toPeer.socketId).emit("receive-video", { url, action, time });
     }
   });
 
-  socket.on("connect-peer", () => {});
-
+  // Remove/kick peer (host can only kick from group)
   socket.on("remove-peer", ({ id }) => {
-    const info = peers.get(id);
-    if (info) {
-      io.to(info.socketId).emit("remove-peer", { id });
-      setTimeout(() => {
-        if (io.sockets.sockets.get(info.socketId)) {
-          io.sockets.sockets.get(info.socketId).disconnect(true);
-        }
-      }, 200);
+    if (groupPeers.get(shortId)?.has(id)) {
+      groupPeers.get(shortId).delete(id);
+      groupPeers.get(id).delete(shortId);
+      // update both
+      [shortId, id].forEach(pid => {
+        const peer = peers.get(pid);
+        if (peer) io.to(peer.socketId).emit("update-peers", buildPeerList(pid));
+      });
+      // Optionally fully disconnect:
+      const targetSocket = peers.get(id)?.socketId;
+      if (targetSocket) {
+        io.to(targetSocket).emit("remove-peer", { id });
+        setTimeout(() => {
+          if (io.sockets.sockets.get(targetSocket))
+            io.sockets.sockets.get(targetSocket).disconnect(true);
+        }, 200);
+      }
     }
   });
 
+  // Clean up
   socket.on("disconnect", () => {
     peers.delete(shortId);
-    socketToShort.delete(socket.id);
-    io.emit("peer-left", { id: shortId });
-
-    if (shortId === hostShortId) {
-      const nextHost = peers.keys().next().value || null;
-      hostShortId = nextHost;
-      io.emit("host", { id: hostShortId });
+    // Remove this peer from anyone's groups
+    for (const groupSet of groupPeers.values()) {
+      groupSet.delete(shortId);
     }
-    io.emit("update-peers", buildPeerList());
+    groupPeers.delete(shortId);
   });
 
-  socket.on("leave", () => {
-    socket.disconnect();
-  });
-
-  socket.on("request-peers", () => {
-    socket.emit("update-peers", buildPeerList());
-  });
-
-  socket.on("get-host", () => {
-    socket.emit("host", { id: hostShortId });
-  });
+  socket.on("leave", () => socket.disconnect());
 });
 
 server.listen(PORT, () => {
